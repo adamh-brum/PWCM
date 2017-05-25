e895ea3dd06d311718e1f02a041c23bbd1463b75
Adam Haycock <adam@Adams-MacBook-Pro.local>
2017-05-25 15:42:31 +0100

    Included Font Awesome, for cool graphics

--- ContentCreator/content-creator/src/App.js
+++ ContentCreator/content-creator/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './font-awesome-4.7.0/css/font-awesome.min.css'
+import FontAwesome from 'react-fontawesome';
 
 class App extends Component {
   render() {
@@ -8,11 +10,14 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to the Design Studio</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Use the buttons below to get started.
         </p>
+        <div>
+          <FontAwesome name='rocket' size='2x' spin/>
+        </div>
       </div>
     );
   }
